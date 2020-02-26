22c72034ab4d4dbc9cc2281a61567614a46d120a
Manxue Li <lmanxue@ucsd.edu>
2020-02-25 19:09:41 -0800

    done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
@@ -19,9 +19,26 @@ function initializePage() {
 // init jQuery gestures  
 function initGestures() {
 	// add gestures listener here
+    $(function () {
+		$(".judge-img").bind("taphold", tapholdHandler);
+
+		function tapholdHandler(e) {
+			var targetIDprefix = e.target.id;
+			console.log("got prefix: "+targetIDprefix);
+			$("#"+targetIDprefix+"-bio").show();
+		}
+	})
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+    $('#rsvpForm').submit(function (e) {
+		e.preventDefault();
+		console.log("submitting form");
+		var rsvpEmail = $("#rsvpEmail").val();
+		$.post('/addRSVP', {rsvpEmail: rsvpEmail}, function (res) {
+			alert("RSVP formsuccessfully submitted!");
+			$('#rsvpEmail').val('');
+		})
+	})
 }
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+    L.mapquest.key = 'tAPGwxpaQMw1inYCn4kQJkqa7fXEFaGk';
+
+    var map = L.mapquest.map('map', {
+        center: [32.879011, -117.235976],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12,
+        zoomControl: false
+    });
+
+    L.marker([32.88, -117.236]).addTo(map);
 }
